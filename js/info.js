eef814a068f919ef46aa55f0fc0c3196bdd8101e
Casper Banemann <casperbanemann@gmail.com>
2025-05-07 12:20:32 +0200

    infojs fix

--- js/info.js
+++ js/info.js
@@ -5,8 +5,8 @@ async function detectServer() {
     const res = await fetch(`${BASE_URL}/health-check`);
     if (!res.ok) throw new Error("Not OK");
   } catch (err) {
-    // fallback to port 5000
-    BASE_URL = "http://127.0.0.1:5000";
+    
+    BASE_URL = "http://127.0.0.1:8080";
   }
 }
 
